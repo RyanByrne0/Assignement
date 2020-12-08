4ef4e1dfb42fdbabfd6d553b9680cd7e2fe62714
RyanByrne <c18430266@mydit.ie>
2020-12-08 23:57:34 +0000

    Unity Project(first commit)

---
+++ Space City/Assets/Scripts/Spawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public int xPos;
+    public int yPos;
+    public int zPos;
+    public GameObject Building;
+    void Start()
+    {
+        Spawning();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void Spawning()
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            xPos = Random.Range(-50, 50);
+            yPos = Random.Range(-5, 5);
+            zPos = Random.Range(-50, 50);
+            Instantiate(Building, new Vector3(xPos, yPos, zPos), Quaternion.identity);
+        }
+    }
+}
