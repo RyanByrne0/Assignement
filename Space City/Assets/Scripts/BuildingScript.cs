233c39b2d51dbdbc1a444d74fe825091a784bf8e
RyanByrne <c18430266@mydit.ie>
2020-12-09 02:08:01 +0000

    Slight change, I changed the buildings to be planets instead

--- Space City/Assets/Scripts/BuildingScript.cs
+++ Space City/Assets/Scripts/BuildingScript.cs
@@ -4,16 +4,48 @@ using UnityEngine;
 
 public class BuildingScript : MonoBehaviour
 {
+    public Vector3 Pos;
 
+    public MeshRenderer[] Renderes;
+
+    public float y;
     void Start()
     {
-        
+        Color ranColor = Random.ColorHSV(0, 1, .8f, 1);
+        Pos = transform.position;
+        y = Random.Range(-1 * Time.deltaTime, 1 * Time.deltaTime);
+        ApplyMaterial(ranColor, 0);
     }
 
- 
-
-    void Update()
+    void ApplyMaterial(Color color, int targetMaterialIndex)
     {
-        
+        Material generatedMat = new Material(Shader.Find("Standard"));
+        generatedMat.SetColor("_Color", color);
+
+        for(int i = 0; i < Renderes.Length; i++)
+        {
+            Renderes[i].material = generatedMat;
+        }
+    }
+
+    public void Movement()
+    {
+       if(transform.position.y < -151)
+        {
+            y = 5 * Time.deltaTime;
+        }
+       else if(transform.position.y > 151)
+        {
+            y = -5 * Time.deltaTime;
+        }
+
+        transform.Translate(0, y, 0);
+        transform.Rotate(0, y, 0);
+    }
+
+    
+    void FixedUpdate()
+    {
+        Movement();
     }
 }
--- Space City/Assets/Scripts/Spawn.cs
+++ Space City/Assets/Scripts/Spawn.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
+    //ints
     public int xPos;
     public int yPos;
     public int zPos;
-    public GameObject Building;
+    int Scale;
+    int yScale;
+    int zScale;
+
+    //Gameobjects
+    public GameObject Planet;
     void Start()
     {
         Spawning();
@@ -21,12 +27,19 @@ public class Spawn : MonoBehaviour
 
     void Spawning()
     {
-        for(int i = 0; i < 100; i++)
+        for(int i = 0; i < 200; i++)
         {
-            xPos = Random.Range(-50, 50);
-            yPos = Random.Range(-5, 5);
-            zPos = Random.Range(-50, 50);
-            Instantiate(Building, new Vector3(xPos, yPos, zPos), Quaternion.identity);
+            //spawn pos of buildings
+            xPos = Random.Range(-2000, 2000);
+            yPos = Random.Range(-150, 150);
+            zPos = Random.Range(-2000, 2000);
+            Instantiate(Planet, new Vector3(xPos, yPos, zPos), Quaternion.identity);
+
+            //Size of buildings
+            Scale = Random.Range(3, 100);
+            //yScale = Random.Range(3, 30);
+            //zScale = Random.Range(3, 15);
+           Planet.transform.localScale = new Vector3(Scale, Scale, Scale);
         }
     }
 }
