8215596c7eaa3e13688131b693d469aa5e990d63
RyanByrne <c18430266@mydit.ie>
2020-12-23 19:13:29 +0000

    Explosion Effect added

---
+++ Space City/Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public AudioSource Explosion;
+
+    public GameObject ExplosionObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Explosion = GameObject.Find("Player").GetComponent<Movement>().audioClips[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, GameObject.Find("Player").GetComponent<Movement>().maxSpeed * Time.deltaTime * 1.30f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Planet"))
+        {
+            Instantiate(ExplosionObject, new Vector3(other.transform.position.x, other.transform.position.y, other.transform.position.z), Quaternion.identity);
+            Explosion.Play();
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Space City/Assets/Scripts/Explosion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    float growthSpeed;
+    float Ran;
+    float ExplodingTime;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ran = Random.Range(5, 15);
+        ExplodingTime = Random.Range(8, 15);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Exploding();
+    }
+
+    void Exploding()
+    {
+        growthSpeed += Ran;
+        
+        transform.localScale = new Vector3(growthSpeed, growthSpeed, growthSpeed);
+
+        ExplodingTime -= Time.deltaTime;
+
+        if(ExplodingTime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+   
+}
